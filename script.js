85ac9be597a03da93e7a6c0c52c15bc81120d472
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-07 18:27:15 +1000

    add hardcoded books into array
    purpose is to test the function I'm about to write to loop through and display those books

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-const myLibrary = [];
+const myLibrary = [
+  {
+    title: "Meditations",
+    author: "Marcus Aurelius",
+    coverUrl: "https://m.media-amazon.com/images/I/81DFDGzHZqL._SY466_.jpg",
+  },
+  {
+    title: "The Book of Five Rings",
+    author: "Miyamoto Musashi",
+    coverUrl:
+      "https://m.media-amazon.com/images/I/61e446u-w2L._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    author: "J.K. Rowling",
+    coverUrl:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg/220px-Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg",
+  },
+];
 
 function Book() {
   // constructor
