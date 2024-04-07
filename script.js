cddfe28bcb7b73fc35788e4e7c58a7f8fba29fdb
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-08 07:52:08 +1000

    add prompt into function to get user input for title and author of books. remove need to capture url of cover image

--- script.js
+++ script.js
@@ -2,28 +2,23 @@ const myLibrary = [
   {
     title: "Meditations",
     author: "Marcus Aurelius",
-    coverUrl: "https://m.media-amazon.com/images/I/81DFDGzHZqL._SY466_.jpg",
   },
   {
     title: "The Book of Five Rings",
     author: "Miyamoto Musashi",
-    coverUrl:
-      "https://m.media-amazon.com/images/I/61e446u-w2L._AC_UF1000,1000_QL80_.jpg",
   },
   {
     title: "Harry Potter and the Philosopher's Stone",
     author: "J.K. Rowling",
-    coverUrl:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg/220px-Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg",
   },
 ];
 
-function Book(title, author, coverUrl) {
+function Book(title, author) {
   this.title = title;
   this.author = author;
-  this.coverUrl = coverUrl;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  const title = prompt("What is the title of the book?");
+  const author = prompt("Who is the author of the book?");
 }
