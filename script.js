f36f9bce410aaf40e7aa1c05ec696612911a8a69
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-09 10:43:54 +1000

    use margin-right to make sure, whether there is or isn't the tick to indicate book is read, the title is always centered

--- script.js
+++ script.js
@@ -58,6 +58,7 @@ function displayBooks() {
     const bookDiv = document.createElement("div");
     bookDiv.classList.add("book");
     bookDiv.setAttribute("array-index", bookIndex);
+    if (book.hasRead) bookDiv.classList.add("read");
 
     const deleteBookBtn = document.createElement("button");
     deleteBookBtn.textContent = "x";
@@ -66,8 +67,12 @@ function displayBooks() {
     deleteBookBtn.addEventListener("click", removeBookFromLibrary);
 
     const bookNameDiv = document.createElement("div");
-    bookNameDiv.classList.add("book-name");
+    const checkIconContainer = document.createElement("figure");
+    const checkIconSvg =
+      '<svg  xmlns="http://www.w3.org/2000/svg"  width="100%"  height="100%"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-circle-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-1.293 5.953a1 1 0 0 0 -1.32 -.083l-.094 .083l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.403 1.403l.083 .094l2 2l.094 .083a1 1 0 0 0 1.226 0l.094 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" /></svg>';
     const bookName = document.createElement("h3");
+    bookNameDiv.classList.add("book-name");
+    checkIconContainer.innerHTML = checkIconSvg;
     bookName.textContent = book.title;
 
     const authorName = document.createElement("small");
@@ -83,15 +88,7 @@ function displayBooks() {
     readBtn.addEventListener("click", toggleReadStatus);
     hasReadDiv.appendChild(readBtn);
 
-    if (book.hasRead) {
-      bookDiv.classList.add("read");
-
-      checkIconContainer = document.createElement("figure");
-      checkIconContainer.innerHTML =
-        '<svg  xmlns="http://www.w3.org/2000/svg"  width="100%"  height="100%"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-circle-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-1.293 5.953a1 1 0 0 0 -1.32 -.083l-.094 .083l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.403 1.403l.083 .094l2 2l.094 .083a1 1 0 0 0 1.226 0l.094 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" /></svg>';
-      bookNameDiv.appendChild(checkIconContainer);
-    }
-
+    bookNameDiv.appendChild(checkIconContainer);
     bookNameDiv.appendChild(bookName);
     bookDiv.appendChild(deleteBookBtn);
     bookDiv.appendChild(bookNameDiv);
